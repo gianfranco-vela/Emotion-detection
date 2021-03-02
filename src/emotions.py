06c2a3b8b2fa5d6d38fc6b3d73be0b2fe73c0271
GianfrancoVela <gianfranco.vela@codecamp-n.com>
2021-03-02 16:49:00 +0100

    adds working gui

--- src/emotions.py
+++ src/emotions.py
@@ -16,34 +16,34 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # command line argument
-ap = argparse.ArgumentParser()
-ap.add_argument("--mode",help="train/display")
-mode = ap.parse_args().mode
+# ap = argparse.ArgumentParser()
+# ap.add_argument("--mode",help="train/display")
+# mode = ap.parse_args().mode
 
 # plots accuracy and loss curves
-def plot_model_history(model_history):
-    """
-    Plot Accuracy and Loss curves given the model_history
-    """
-    fig, axs = plt.subplots(1,2,figsize=(15,5))
-    # summarize history for accuracy
-    axs[0].plot(range(1,len(model_history.history['accuracy'])+1),model_history.history['accuracy'])
-    axs[0].plot(range(1,len(model_history.history['val_accuracy'])+1),model_history.history['val_accuracy'])
-    axs[0].set_title('Model Accuracy')
-    axs[0].set_ylabel('Accuracy')
-    axs[0].set_xlabel('Epoch')
-    axs[0].set_xticks(np.arange(1,len(model_history.history['accuracy'])+1),len(model_history.history['accuracy'])/10)
-    axs[0].legend(['train', 'val'], loc='best')
-    # summarize history for loss
-    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
-    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
-    axs[1].set_title('Model Loss')
-    axs[1].set_ylabel('Loss')
-    axs[1].set_xlabel('Epoch')
-    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
-    axs[1].legend(['train', 'val'], loc='best')
-    fig.savefig('plot.png')
-    plt.show()
+# def plot_model_history(model_history):
+#     """
+#     Plot Accuracy and Loss curves given the model_history
+#     """
+#     fig, axs = plt.subplots(1,2,figsize=(15,5))
+#     # summarize history for accuracy
+#     axs[0].plot(range(1,len(model_history.history['accuracy'])+1),model_history.history['accuracy'])
+#     axs[0].plot(range(1,len(model_history.history['val_accuracy'])+1),model_history.history['val_accuracy'])
+#     axs[0].set_title('Model Accuracy')
+#     axs[0].set_ylabel('Accuracy')
+#     axs[0].set_xlabel('Epoch')
+#     axs[0].set_xticks(np.arange(1,len(model_history.history['accuracy'])+1),len(model_history.history['accuracy'])/10)
+#     axs[0].legend(['train', 'val'], loc='best')
+#     # summarize history for loss
+#     axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+#     axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+#     axs[1].set_title('Model Loss')
+#     axs[1].set_ylabel('Loss')
+#     axs[1].set_xlabel('Epoch')
+#     axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+#     axs[1].legend(['train', 'val'], loc='best')
+#     fig.savefig('plot.png')
+#     plt.show()
 
 # Define data generators
 train_dir = 'data/train'
@@ -89,70 +89,73 @@ model.add(Flatten())
 model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(7, activation='softmax'))
+stop_executing = False
 
 
 def take_screenshot(faceshot, i, m):
     images_dir = './screenshots/'
     screenshot = pyautogui.screenshot()
-    cv2.imwrite(images_dir + '{iteration}_face_{emotion}.png'.format(emotion=m, iteration=i), faceshot)
+    # cv2.imwrite(images_dir + '{iteration}_face_{emotion}.png'.format(emotion=m, iteration=i), faceshot)
     screenshot.save(images_dir + '{iteration}_screen_{emotion}.png'.format(emotion=m, iteration=i))
 
 
-# If you want to train the same model or try other models, go for this
-if mode == "train":
-    model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
-    model_info = model.fit(
+def start_detection(mode):
+    # If you want to train the same model or try other models, go for this
+    if mode == "train":
+        model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
+        model_info = model.fit(
             train_generator,
             steps_per_epoch=num_train // batch_size,
             epochs=num_epoch,
             validation_data=validation_generator,
             validation_steps=num_val // batch_size)
-    plot_model_history(model_info)
-    model.save_weights('model.h5')
+        # plot_model_history(model_info)
+        model.save_weights('model.h5')
 
-# emotions will be displayed on your face from the webcam feed
-elif mode == "display":
-    model.load_weights('model.h5')
+    # emotions will be displayed on your face from the webcam feed
+    elif mode == "display":
+        model.load_weights('model.h5')
 
-    # prevents openCL usage and unnecessary logging messages
-    cv2.ocl.setUseOpenCL(False)
+        # prevents openCL usage and unnecessary logging messages
+        cv2.ocl.setUseOpenCL(False)
 
-    # dictionary which assigns each label an emotion (alphabetical order)
-    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+        # dictionary which assigns each label an emotion (alphabetical order)
+        emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
-    # start the webcam feed
-    cap = cv2.VideoCapture(0)
-    tempMaxIndex = 4
-    iteration = 0
-    while True:
-        # Find haar cascade to draw bounding box around face
-        ret, frame = cap.read()
-        if not ret:
-            break
-        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+        # start the webcam feed
+        cap = cv2.VideoCapture(0)
+        tempMaxIndex = 4
+        iteration = 0
+        while True:
+            # Find haar cascade to draw bounding box around face
+            ret, frame = cap.read()
+            if not ret:
+                break
+            facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
-        for (x, y, w, h) in faces:
+            for (x, y, w, h) in faces:
 
-            cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
+                cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+                roi_gray = gray[y:y + h, x:x + w]
+                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+                prediction = model.predict(cropped_img)
+                maxindex = int(np.argmax(prediction))
 
-            # Take Screenshot when maxindex changes
-            # get current emotion so it won't update while saving image
-            emotion = emotion_dict[tempMaxIndex]
-            if tempMaxIndex != maxindex:
-                tempMaxIndex = maxindex
-                take_screenshot(frame, iteration, emotion)
-            cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),
-                        2, cv2.LINE_AA)
-        iteration += 1
-        #cv2.imshow('Video', cv2.resize(frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+                # Take Screenshot when maxindex changes
+                # get current emotion so it won't update while saving image
+                emotion = emotion_dict[tempMaxIndex]
+                if tempMaxIndex != maxindex:
+                    tempMaxIndex = maxindex
+                    take_screenshot(frame, iteration, emotion)
+                cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                            (255, 255, 255),
+                            2, cv2.LINE_AA)
+            iteration += 1
+            # cv2.imshow('Video', cv2.resize(frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
+            if stop_executing:
+                break
 
-    cap.release()
-    cv2.destroyAllWindows()
+        cap.release()
+        cv2.destroyAllWindows()
--- src/gui.py
+++ src/gui.py
@@ -1,15 +1,28 @@
 import sciter
-import os
+import src.emotions as es
+import threading
 
+
+class Frame(sciter.Window):
+
+    def __init__(self):
+        self.detection_thread = threading.Thread(target=es.start_detection, args=('display',), daemon=True)
+        super().__init__(ismain=True, uni_theme=True, debug=False)
+
+    @sciter.script('start')
+    def start_script(self):
+        self.detection_thread.start()
+        print('Starting Emotion-Detection....')
+
+    @sciter.script('stop')
+    def stop_script(self):
+        es.stop_executing = True
+        self.detection_thread.join()
+        print('Stopping Emotion-Detection....')
 
 
 def get_main_window():
-    frame = sciter.Window(ismain=True, uni_theme=True)
+    frame = Frame()
     frame.load_file("./gui/minimal.htm")
     frame.expand()
     frame.run_app()
-
-
-def start_script():
-    ## Start Recognition
-    os.system('python emotions.py --mode display')
